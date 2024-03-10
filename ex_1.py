b652f1e3d322fb1e2a32ef926cd54683ccf16d50
Teleshun <108687533+Teleshun@users.noreply.github.com>
2024-03-10 15:43:09 +0200

    Update ex_1.py

--- ex_1.py
+++ ex_1.py
@@ -1,13 +1,21 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    specified_date = datetime.strptime(date, '%Y-%m-%d')
-    current_datetime = datetime.today()
-    date_difference = current_datetime - specified_date
-    return date_difference.days
-
+    try:
+        specified_date = datetime.strptime(date, '%Y-%m-%d')
+        current_datetime = datetime.today()
+        date_difference = current_datetime - specified_date
+        return date_difference.days
+    except ValueError as e:
+        print(f"Помилка: {e}")
+        return None
+    
 specified_date_str = "2020-10-09"
 result = get_days_from_today(specified_date_str)
-print(f'Різниця складає {result} днів')
+
+if result is not None:
+    print(f'Різниця складає {result} днів')
+else:
+    print('Не вдалося обчислити різницю через некоректні вхідні дані.')
 
 
